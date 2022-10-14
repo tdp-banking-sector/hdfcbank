ef5b83dd644f2089e5f3ac7192008bcfd2899558
tdilip411 <tdilip411@gmail.com>
2022-10-14 15:53:33 +0530

    Modified java code

--- DBUtils.java
+++ DBUtils.java
@@ -1,1 +1,7 @@
 Hello everyone this file is used for echoing the sound into soudnbar.
+public class HelloWorld{
+
+     public static void main(String []args){
+        System.out.println("Hello, World!");
+     }
+}
